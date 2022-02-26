f278c8211446af530b92729ea070a6d364178019
Gpscoolx <91938287+Gpscoolx@users.noreply.github.com>
2022-02-26 21:22:54 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('🧩 Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('😍𝙷𝙴𝙻𝙿💌', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂', url='https://t.me/TeamEvamaria')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -51,7 +51,7 @@ async def save_group(bot, message):
                     await (temp.MELCOW['welcome']).delete()
                 except:
                     pass
-            temp.MELCOW['welcome'] = await message.reply(f"<b>Hai👋 , {u.mention}, Thanks for joining💐 {message.chat.title} We would like to welcome you to our team😍 and extremely happy to have you as one of us🌷. </b>")
+            temp.MELCOW['welcome'] = await message.reply(f"<b>𝓗𝓪𝓲👋 , {u.mention}, Thanks for joining💐 {message.chat.title} We would like to welcome you to our team😍 and extremely happy to have you as one of us🌷. </b>")
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
